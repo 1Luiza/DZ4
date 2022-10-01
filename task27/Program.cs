0bc7f90406eab494104b5c538bf79bdc86b2a2c3
Луиза Анисимова <monalyiza@mail.ru>
2022-10-01 13:13:26 +0900

    решение задач 25, 27, 29

---
+++ task27/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+452 -> 11
+
+82 -> 10
+
+9012 -> 12
+*/
+
+int sumOfDigit(int userNumber)
+{
+    int sum = 0;
+    while (userNumber > 0)
+    {
+        int currentDigit = userNumber % 10;
+        sum = sum + currentDigit;
+        userNumber = userNumber / 10;
+    } 
+    return sum;    
+}
+
+Console.WriteLine("Введите число: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+userNumber = Math.Abs(userNumber);
+int result = sumOfDigit(userNumber);
+Console.WriteLine(result);
---
+++ task29/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Напишите программу, в которой пользователь задает длину массива, 
+элементы которого задаются в диапазоне [1,99] и выводит на экран .
+
+5 -> [1, 2, 5, 7, 19]
+
+3 -> [6, 1, 33]
+*/
+
+void printArray (int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1, 99);
+        Console.Write(array[i]);
+        if(i < array.Length - 1)
+        {
+            Console.Write(",");
+        }
+    }
+    Console.WriteLine("]");
+}
+
+Console.WriteLine("Введите длину массива: ");
+int[] array = new int [Convert.ToInt32(Console.ReadLine())];
+printArray(array);
